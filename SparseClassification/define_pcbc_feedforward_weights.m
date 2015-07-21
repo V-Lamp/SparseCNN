567b952862702e3a5735c07b46162d54376ad03f
VLabridis <basilis.lab@gmail.com>
2015-07-21 16:08:35 +0100

    remove all unused folders
    To be loaded on branch, on demand

--- SparseClassification/define_pcbc_feedforward_weights.m
+++
@@ -1,3 +0,0 @@
-function W=define_pcbc_feedforward_weights(V)
-%for PCBC also need to define a set of feedforward (descriminative) weights
-W=norm_dictionary(V,1);%feedforward weights equal to feedback weights normalized by sum
