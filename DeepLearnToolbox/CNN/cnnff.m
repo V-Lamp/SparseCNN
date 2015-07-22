83b7b45128ecc82f88c4f80ebb2dddbbfd546e61
VLabridis <basilis.lab@gmail.com>
2015-07-22 04:54:12 +0100

    changed FF to use aggConv

--- DeepLearnToolbox/CNN/cnnff.m
+++ DeepLearnToolbox/CNN/cnnff.m
@@ -6,19 +6,26 @@ function net = cnnff(net, x)
     for l = 2 : n   %  for each layer
         if strcmp(net.layers{l}.type, 'c')
             %  !!below can probably be handled by insane matrix operations
+            if  ~isfield(net.layers{l}, 'a')
+                net.layers{l}.a={};
+            end
+            net.layers{l}.a = aggConv(...
+                net.layers{l - 1}.a, ...
+                net.layers{l}.k.', ...
+                'valid',net.layers{l}.a);
             for j = 1 : net.layers{l}.outputmaps   %  for each output map
-                %  create temp output map
-                z = zeros(...
-                    size(net.layers{l - 1}.a{1}) - ...
-                    [net.layers{l}.kernelsize - 1 net.layers{l}.kernelsize - 1 0]);
-                for i = 1 : inputmaps   %  for each input map
-                    %  convolve with corresponding kernel and add to temp output map                    
-                    z = z + convn(...
-                        net.layers{l - 1}.a{i}, ...
-                        net.layers{l}.k{i,j}, 'valid');
-                end
+%                 %  create temp output map
+%                 z = zeros(...
+%                     size(net.layers{l - 1}.a{1}) - ...
+%                     [net.layers{l}.kernelsize - 1 net.layers{l}.kernelsize - 1 0]);
+%                 for i = 1 : inputmaps   %  for each input map
+%                     %  convolve with corresponding kernel and add to temp output map                    
+%                     z = z + convn(...
+%                         net.layers{l - 1}.a{i}, ...
+%                         net.layers{l}.k{i,j}, 'valid');
+%                 end
                 %  add bias, pass through nonlinearity
-                net.layers{l}.a{j} = sigm(z + net.layers{l}.b{j});
+                net.layers{l}.a{j} = sigm( net.layers{l}.a{j} + net.layers{l}.b{j});
             end
             %  set number of input maps to this layers number of outputmaps
             inputmaps = net.layers{l}.outputmaps;
